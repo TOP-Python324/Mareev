4c6449567994145eaa03d6426efdcfbf5e480572
Dmitry Mareev <74976040+qwe123rty1996@users.noreply.github.com>
2023-05-27 14:42:11 +0300

    Задание 5 v2

--- 2023.05.21/1.5.py
+++ 2023.05.21/1.5.py
@@ -1,7 +1,11 @@
-mile = float(input("Введите количество миль: "))
-const = 1.61
-km = mile * const
+mile_whole = int(input("Введите челую часть: "))
+mile_fractional = int(input("Введите дробную часть: "))
+mile = f"{mile_whole}.{mile_fractional}"
+mile = float(mile)
+const_km = 1.61
+km = mile * const_km
 print(f'{mile} миль = {km:.1f} км')
 
-# Введите количество миль: 12.4
-# 12.4 миль = 20.0 км
+# Введите челую часть: 12
+# Введите дробную часть: 6
+# 12.6 миль = 20.3 км
