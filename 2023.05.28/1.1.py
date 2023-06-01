4f13a548003f963f61562145971c5b2991c89f28
Dmitry Mareev <74976040+qwe123rty1996@users.noreply.github.com>
2023-06-01 20:10:08 +0300

    Задание 1

---
+++ 2023.05.28/1.1.py
@@ -0,0 +1,19 @@
+number_1 = float(input("Введите первое число:"))
+number_2 = float(input("Введите второе число:"))
+number_3 = float(input("Введите третье число:"))
+
+total = 0;
+
+if number_1 > 0:
+    total += number_1
+if number_2 > 0:
+    total += number_2
+if number_3 > 0:
+    total += number_3
+    
+print(total)
+
+# Введите первое число:3
+# Введите второе число:-2
+# Введите третье число:11.5
+# 14.5
