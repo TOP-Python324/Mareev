38c7d381fcff25cbde6e6fcd6f0f8a0746b77204
Dmitry Mareev <74976040+qwe123rty1996@users.noreply.github.com>
2023-06-10 11:51:01 +0300

    Задание 8

---
+++ 2023.06.04/1.8.py
@@ -0,0 +1,12 @@
+fib1 = fib2 = 1
+
+number = int(input("Введите число: "))
+
+print(fib1, fib2, end=' ')
+
+for i in range(2, number):
+    fib1, fib2 = fib2, fib1 + fib2
+    print(fib2, end=' ')
+
+# Введите число: 14
+# 1 1 2 3 5 8 13 21 34 55 89 144 233 377
