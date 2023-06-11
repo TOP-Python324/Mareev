23fac1ffe3670c26ea6f7644d32ebc8b40fb9459
Gennadiy <renardes@gmail.com>
2023-06-11 10:27:37 +0500

    правки удобства читаемости кода

--- 2023.06.04/1.py
+++ 2023.06.04/1.py
@@ -1,10 +1,11 @@
 total = []
-while (number := int(input("Введите число: "))):
+while number := int(input("Введите число: ")):
     if number % 7 != 0:
         break
     else:
         total.append(number)
 print(*reversed(total))
+
 
 # Введите число: 7
 # Введите число: 14
@@ -14,3 +15,5 @@ print(*reversed(total))
 # Введите число: 56
 # Введите число: 22
 # 56 49 28 21 14 7
+
+
--- 2023.06.04/4.py
+++ 2023.06.04/4.py
@@ -1,6 +1,7 @@
 n = int(input("Введите количество разрядов: "))
 num = "9" * n
 lst = []
+# ПЕРЕИМЕНОВАТЬ: имена переменных i, j, k используются только для индексов, а здесь вы перебираете числа и их делители
 for i in range(2, int(num)):
     for j in range(2, i):
         if i % j == 0:
@@ -8,10 +9,13 @@ for i in range(2, int(num)):
         elif len(str(i)) != n:
             break
     else:
-     lst.append((i))
+        lst.append(i)
 if n > 1:
     lst.remove(2)
 print(len(lst))
 
+
 # Введите количество разрядов: 4
 # 1061
+
+
