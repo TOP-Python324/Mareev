9caea4c7fe2140298620a82ab30fb0c354e6fdb1
Dmitry Mareev <74976040+qwe123rty1996@users.noreply.github.com>
2023-06-20 21:07:14 +0300

    Задание 5

---
+++ 2023.06.11/5.py
@@ -0,0 +1,17 @@
+scores_letters = {
+    1: 'АВЕИНОРСТ',
+    2: 'ДКЛМПУ',
+    3: 'БГЬЯ',
+    4: 'ЙЫ',
+    5: 'ЖЗХЦЧ',
+    8: 'ФШЭЮ',
+    10: 'Щ',
+    15: 'Ъ'
+}
+result = 0
+text = input("Введите слово: ").upper()
+for key, value in scores_letters.items():
+    for el in text:
+        if el in value:
+            result += key
+print(result)
