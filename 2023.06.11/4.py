ae6d87fdeb9ef35f20bf783c859f458c19f9ad97
Dmitry Mareev <74976040+qwe123rty1996@users.noreply.github.com>
2023-06-21 08:06:00 +0300

    Задание 4

--- 2023.06.11/4.py
+++ 2023.06.11/4.py
@@ -3,7 +3,6 @@ while (meaning := input("Введите ключ и значение: ")):
   meaning = meaning.split(" ")
   for i in range(len(meaning)):
       meaning_dict[int(meaning[i])] = meaning[i + 1]
-      print(meaning_dict)
       break
 val = input("Введите значение: ")
 for key, value in meaning_dict.items():
@@ -13,3 +12,12 @@ for key, value in meaning_dict.items():
     elif val not in meaning_dict.values():
         result = "! value error !"
 print(result)
+
+# Введите ключ и значение: 100 x
+# Введите ключ и значение: 445 r
+# Введите ключ и значение: 111 h
+# Введите ключ и значение: 345 k
+# Введите ключ и значение: 664 f
+# Введите ключ и значение:
+# Введите значение: h
+# 111
